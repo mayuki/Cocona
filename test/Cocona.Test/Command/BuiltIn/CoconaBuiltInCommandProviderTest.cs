135191e72452c9968d9a4246c11ab25bb4da35bf
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-01-15 21:36:11 +0900

    Add a test.

--- test/Cocona.Test/Command/BuiltIn/CoconaBuiltInCommandProviderTest.cs
+++ test/Cocona.Test/Command/BuiltIn/CoconaBuiltInCommandProviderTest.cs
@@ -20,6 +20,18 @@ namespace Cocona.Test.Command.BuiltIn
             commands.Primary.Should().NotBeNull();
         }
 
+        [Fact]
+        public void RewriteCommandNamesAsLowerCase()
+        {
+            var provider = new CoconaBuiltInCommandProvider(new CoconaCommandProvider(new[] { typeof(CommandTestBuiltInPrimaryCommand) }));
+            var commands = provider.GetCommandCollection();
+            commands.Should().NotBeNull();
+            commands.All.Should().HaveCount(3); // A, B, BuiltInPrimary
+            commands.All[0].Name.Should().Be("a");
+            commands.All[1].Name.Should().Be("b");
+            commands.Primary.Should().NotBeNull();
+        }
+
         public class CommandTestBuiltInPrimaryCommand
         {
             public void A() { }
