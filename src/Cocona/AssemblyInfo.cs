1b8de14fb1b3bdd9e88487e18c3f074eb378f8f8
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-01-18 08:48:32 +0900

    Sign assemblies with a strong name.

--- src/Cocona/AssemblyInfo.cs
+++ src/Cocona/AssemblyInfo.cs
@@ -1,6 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-[assembly: InternalsVisibleTo("Cocona.Test")]
+[assembly: InternalsVisibleTo(
+    "Cocona.Test, PublicKey=" +
+    "0024000004800000940000000602000000240000525341310004000001000100a9b5075915b507" +
+    "2980fe4a809e4c0e324df70a606e904da38bf1761f6418d6f168c103bb982660cf73c74e74406e" +
+    "fe256bebfb34cdfb8a63b56f749f745e1cbb9338aea418e8537b81dbcfb7d3391b44bb52670472" +
+    "e629990fee2d2f810bbffa1cd0e19bc59fe7da5b562bdc874cbd3382dbf6992a7f41784e7f6ed8" +
+    "b0e271ca"
+)]
