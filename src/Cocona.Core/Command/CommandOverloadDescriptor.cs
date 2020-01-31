7f41565eb1d7632d42bf25ffeff0d6443560a216
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-01-31 22:57:05 +0900

    Move to CommandOverloadDescriptor.cs

---
+++ src/Cocona.Core/Command/CommandOverloadDescriptor.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace Cocona.Command
+{
+    public class CommandOverloadDescriptor
+    {
+        public CommandOptionDescriptor Option { get; }
+        public string? Value { get; }
+        public CommandDescriptor Command { get; }
+        public IEqualityComparer<string> Comparer { get; }
+
+        public CommandOverloadDescriptor(CommandOptionDescriptor option, string? value, CommandDescriptor command, IEqualityComparer<string>? comparer)
+        {
+            Option = option ?? throw new ArgumentNullException(nameof(option));
+            Value = value;
+            Command = command ?? throw new ArgumentNullException(nameof(command));
+            Comparer = comparer ?? StringComparer.OrdinalIgnoreCase;
+        }
+    }
+}
