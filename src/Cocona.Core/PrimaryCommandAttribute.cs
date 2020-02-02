479f779065a84cad14625d2e7df736342a4fa515
Benjamin Evenson <bevenson@gmail.com>
2020-02-02 17:46:07 +1000

    Correct typo in PrimaryCommandAttribute comment

--- src/Cocona.Core/PrimaryCommandAttribute.cs
+++ src/Cocona.Core/PrimaryCommandAttribute.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace Cocona
 {
     /// <summary>
-    /// Specifies the method that should be treated as a parimary command.
+    /// Specifies the method that should be treated as a primary command.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public sealed class PrimaryCommandAttribute : Attribute
