a06c456b4386abdf41a6722d1211b3a622014608
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-05-27 08:40:13 +0900

    Add comment

--- src/Cocona.Core/IgnoreUnknownOptionsAttribute.cs
+++ src/Cocona.Core/IgnoreUnknownOptionsAttribute.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Cocona
 {
+    /// <summary>
+    /// Specifies the command that should ignore unknown options.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
     public class IgnoreUnknownOptionsAttribute : Attribute
     {
