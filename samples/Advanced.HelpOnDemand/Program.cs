b96ebd1553e7e76aec7ea918e81de5bfed4c7416
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-07-12 00:10:54 +0900

    Add a sample project

---
+++ samples/Advanced.HelpOnDemand/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using Cocona;
+using Cocona.Help;
+
+namespace CoconaSample.Advanced.HelpOnDemand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoconaApp.Run<Program>(args);
+        }
+
+        public void ForContext(bool optionA, [FromService] ICoconaHelpMessageBuilder helpMessageBuilder)
+        {
+            // Show commands help. (same as `./HelpOnDemand --help`)
+            Console.WriteLine(helpMessageBuilder.BuildAndRenderForCurrentContext());
+        }
+
+        public void ForCommand(bool optionA, [FromService] ICoconaHelpMessageBuilder helpMessageBuilder)
+        {
+            // Show a help for this command. (same as `./HelpOnDemand for-command --help`)
+            Console.WriteLine(helpMessageBuilder.BuildAndRenderForCurrentCommand());
+        }
+    }
+}
