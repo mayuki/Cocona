841d6ad37242153a8022679205cdac7e23508614
Mayuki Sawatari <mayuki+github@misuzilla.org>
2022-01-08 00:41:52 +0900

    Run unit tests on .NET Core 3.1

---
+++ test/Cocona.Test/Shims/System.Runtime.CompilerServices.IsExternalInit.cs
@@ -0,0 +1,14 @@
+#if !NET5_0_OR_GREATER
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices
+{
+    internal class IsExternalInit : Attribute
+    {
+    }
+}
+#endif
