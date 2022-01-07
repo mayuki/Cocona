55eb41ec3b9a61ce7424fef5f57c2798b491d6dd
Mayuki Sawatari <mayuki+github@misuzilla.org>
2022-01-08 00:24:12 +0900

    Fix compatible implementation of getting nullable annotations

--- src/Cocona.Core/Internal/NullabilityInfoContextHelper.cs
+++ src/Cocona.Core/Internal/NullabilityInfoContextHelper.cs
@@ -27,6 +27,12 @@ namespace Cocona.Internal
 #else
         public NullabilityState GetNullabilityState(ParameterInfo parameterInfo)
         {
+            return GetNullabilityState_Compat(parameterInfo);
+        }
+#endif
+
+        internal NullabilityState GetNullabilityState_Compat(ParameterInfo parameterInfo)
+        {
             if (parameterInfo.ParameterType.IsValueType)
             {
                 if (parameterInfo.ParameterType.IsConstructedGenericType && parameterInfo.ParameterType.GetGenericTypeDefinition() == typeof(Nullable<>))
@@ -39,16 +45,47 @@ namespace Cocona.Internal
                 }
             }
 
+            // NullableAttribute on the parameter.
             foreach (var attr in parameterInfo.GetCustomAttributesData())
             {
                 if (attr.AttributeType is { Namespace: "System.Runtime.CompilerServices", Name: "NullableAttribute" })
                 {
-                    return (NullabilityState)((byte)attr.ConstructorArguments[0].Value!);
+                    return (NullabilityState)GetFirstByte(attr.ConstructorArguments[0].Value!);
+                }
+            }
+
+            // NullableContextAttribute on the member
+            foreach (var attr in parameterInfo.Member.GetCustomAttributesData())
+            {
+                if (attr.AttributeType is { Namespace: "System.Runtime.CompilerServices", Name: "NullableContextAttribute" })
+                {
+                    return (NullabilityState)GetFirstByte(attr.ConstructorArguments[0].Value!);
+                }
+            }
+
+            // NullableContextAttribute on the declaring type
+            if (parameterInfo.Member.DeclaringType is not null)
+            {
+                foreach (var attr in parameterInfo.Member.DeclaringType.GetCustomAttributesData())
+                {
+                    if (attr.AttributeType is { Namespace: "System.Runtime.CompilerServices", Name: "NullableContextAttribute" })
+                    {
+                        return (NullabilityState)GetFirstByte(attr.ConstructorArguments[0].Value!);
+                    }
                 }
             }
 
             return NullabilityState.Unknown;
+
+            static byte GetFirstByte(object? attrCtorArg)
+            {
+                return attrCtorArg switch
+                {
+                    byte x => x,
+                    IReadOnlyCollection<CustomAttributeTypedArgument> x => (byte)x.First().Value!,
+                    _ => throw new NotSupportedException(),
+                };
+            }
         }
-#endif
     }
 }
--- test/Cocona.Test/Internal/NullabilityInfoContextHelperTest.cs
+++ test/Cocona.Test/Internal/NullabilityInfoContextHelperTest.cs
@@ -68,5 +68,82 @@ namespace Cocona.Test.Internal
             helper.GetNullabilityState(a.Method.GetParameters()[0]).Should().Be(NullabilityInfoContextHelper.NullabilityState.Unknown);
 #nullable restore
         }
+
+        [Fact]
+        public void Compat_ValueType_Nullable()
+        {
+            var helper = new NullabilityInfoContextHelper();
+            Action<int?> a = (int? value) => { };
+            helper.GetNullabilityState_Compat(a.Method.GetParameters()[0]).Should().Be(NullabilityInfoContextHelper.NullabilityState.Nullable);
+        }
+
+        [Fact]
+        public void Compat_ValueType_NotNull()
+        {
+            var helper = new NullabilityInfoContextHelper();
+            Action<int> a = (int value) => { };
+            helper.GetNullabilityState_Compat(a.Method.GetParameters()[0]).Should().Be(NullabilityInfoContextHelper.NullabilityState.NotNull);
+        }
+
+
+        [Fact]
+        public void Compat_NullableReferenceType_Enable_Nullable()
+        {
+#nullable enable
+            var helper = new NullabilityInfoContextHelper();
+            Action<string?> a = (string? value) => { };
+            helper.GetNullabilityState_Compat(a.Method.GetParameters()[0]).Should().Be(NullabilityInfoContextHelper.NullabilityState.Nullable);
+#nullable restore
+        }
+
+        [Fact]
+        public void Compat_NullableReferenceType_Enable_Nullable_Array()
+        {
+#nullable enable
+            var helper = new NullabilityInfoContextHelper();
+            Action<string[]?> a = (string[]? value) => { };
+            helper.GetNullabilityState_Compat(a.Method.GetParameters()[0]).Should().Be(NullabilityInfoContextHelper.NullabilityState.Nullable);
+#nullable restore
+        }
+
+        [Fact]
+        public void Compat_NullableReferenceType_Enable_NotNull()
+        {
+#nullable enable
+            var helper = new NullabilityInfoContextHelper();
+            Action<string> a = (string value) => { };
+            helper.GetNullabilityState_Compat(a.Method.GetParameters()[0]).Should().Be(NullabilityInfoContextHelper.NullabilityState.NotNull);
+#nullable restore
+        }
+
+        [Fact]
+        public void Compat_NullableReferenceType_Disable_Nullable_NullableContextAttribute()
+        {
+#nullable disable
+            var helper = new NullabilityInfoContextHelper();
+            Action<string?> a = /*[NullableContext(0x2)]*/(string? value) => { };
+            helper.GetNullabilityState_Compat(a.Method.GetParameters()[0]).Should().Be(NullabilityInfoContextHelper.NullabilityState.Nullable);
+#nullable restore
+        }
+
+        [Fact]
+        public void Compat_NullableReferenceType_Disable_Nullable_NullableAttribute()
+        {
+#nullable disable
+            var helper = new NullabilityInfoContextHelper();
+            Action<string?, string> a = (/*[Nullable(0x2)]*/string? value, string valueB) => { };
+            helper.GetNullabilityState_Compat(a.Method.GetParameters()[0]).Should().Be(NullabilityInfoContextHelper.NullabilityState.Nullable);
+#nullable restore
+        }
+
+        [Fact]
+        public void Compat_NullableReferenceType_Disable_NotNull()
+        {
+#nullable disable
+            var helper = new NullabilityInfoContextHelper();
+            Action<string> a = (string value) => { };
+            helper.GetNullabilityState_Compat(a.Method.GetParameters()[0]).Should().Be(NullabilityInfoContextHelper.NullabilityState.Unknown);
+#nullable restore
+        }
     }
 }
