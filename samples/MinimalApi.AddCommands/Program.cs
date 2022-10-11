945cd7dbf8a2f7903c6efbb88390c754bd005307
Johannes Norrbacka <johannes.norrbacka@hotmail.com>
2022-10-11 19:48:06 +0200

    Updating example

--- samples/MinimalApi.AddCommands/Program.cs
+++ samples/MinimalApi.AddCommands/Program.cs
@@ -2,11 +2,6 @@ using Cocona;
 CoconaApp
 .CreateBuilder()
 .Build()
-.AddCommands
-(
-    ("hello", (string name) => Console.WriteLine($"Hello {name}!")),
-    ("bye", (string name) => Console.WriteLine($"Bye {name}!"))
-)
-.AddCommands(("foo", () => Console.WriteLine("Bar")))
-.AsCoconaApp()
+.AddCommand("hello", (string name) => Console.WriteLine($"Hello {name}!"))
+.AddCommand("bye", (string name) => Console.WriteLine($"Bye {name}!"))
 .Run();
