3ee218b4025d1de49c8e85fe0e7417d983435834
Moreal <dev.moreal@gmail.com>
2022-11-21 00:10:58 +0900

    Support `OptionLikeCommand`'s description

--- src/Cocona.Core/OptionLikeCommandAttribute.cs
+++ src/Cocona.Core/OptionLikeCommandAttribute.cs
@@ -19,13 +19,15 @@ namespace Cocona
         public IReadOnlyList<char> ShortNames { get; }
         public Type CommandType { get; }
         public string CommandMethodName { get; }
+        public string? Description { get; }
 
-        public OptionLikeCommandAttribute(string optionName, char[] shortNames, Type commandType, string commandMethodName)
+        public OptionLikeCommandAttribute(string optionName, char[] shortNames, Type commandType, string commandMethodName, string? description = null)
         {
             OptionName = optionName ?? throw new ArgumentNullException(nameof(optionName));
             ShortNames = shortNames ?? throw new ArgumentNullException(nameof(shortNames));
             CommandType = commandType ?? throw new ArgumentNullException(nameof(commandType));
             CommandMethodName = commandMethodName ?? throw new ArgumentNullException(nameof(commandMethodName));
+            Description = description;
         }
 
         public ICommandData GetCommandData()
@@ -36,7 +38,9 @@ namespace Cocona
                 throw new InvalidOperationException(string.Format(Strings.OptionLikeCommand_MethodNotFound, CommandMethodName, CommandType));
             }
 
-            return new DelegateCommandData(methodInfo, null, new [] { new CommandNameMetadata(OptionName) }.Concat(methodInfo.GetCustomAttributes(inherit: true)).ToArray());
+            return new DelegateCommandData(methodInfo, null, new [] { new CommandNameMetadata(OptionName) }
+                .Concat(Description is { } description ? new object[] { new CommandDescriptionMetadata(description) } : new object[0])
+                .Concat(methodInfo.GetCustomAttributes(inherit: true)).ToArray());
         }
     }
 }
